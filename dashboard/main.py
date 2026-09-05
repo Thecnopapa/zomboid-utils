6c17b99840b8657c0f40307cce226243e5aebd22
iain@lavander <iainvisa@gmail.com>
2026-09-05 17:07:10 +0000

    http to https

--- dashboard/main.py
+++ dashboard/main.py
@@ -58,7 +58,12 @@ def restart_server(server):
         return "", 403
 
 
-
+@app.before_request
+def before_request():
+    if not request.is_secure:
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
 
 
 
